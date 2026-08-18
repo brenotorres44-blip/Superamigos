01e15983c7358e6f4188b5c197e01eca8ea94f53
brenotorres44-blip <brenotorres44@gmail.com>
2026-08-18 14:18:29 -0300

    Update anuncio.js

--- js/anuncio.js
+++ js/anuncio.js
@@ -3,7 +3,7 @@
 //  Gerador de anúncios de ajuda
 // ══════════════════════════════════════════════
 import { familias, anuncioPessoa, anuncioFamilia,
-         setAnuncio, toast, calcIdade } from './utils.js?v=3';
+         setAnuncio, toast, calcIdade } from './utils.js?v=4';
 
 window.abrirAnuncio = (p, f) => {
   setAnuncio(p, f);
