7c04e31bd323daa8a71abc2e3d48f52f9f5c0e38
brenotorres44-blip <brenotorres44@gmail.com>
2026-08-18 15:03:16 -0300

    Update import version for iniciarListenerSacolinhas

--- js/auth.js
+++ js/auth.js
@@ -4,7 +4,7 @@
 import { auth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from './firebase.js?v=4';
 import { unsubFamilias, setUnsubFamilias, toast } from './utils.js?v=4';
 import { iniciarListenerFamilias } from './app.js?v=4';
-import { iniciarListenerSacolinhas } from './sacolinhas.js?v=6';
+import { iniciarListenerSacolinhas } from './sacolinhas.js?v=7';
 
 onAuthStateChanged(auth, user => {
   document.getElementById('loading-screen').style.display = 'none';
