cc10da11dedc6970354c99083673176d3d3400e9
brenotorres44-blip <brenotorres44@gmail.com>
2026-08-18 15:13:22 -0300

    Update import statement for sacolinhas.js

--- js/auth.js
+++ js/auth.js
@@ -4,7 +4,7 @@
 import { auth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from './firebase.js';
 import { unsubFamilias, setUnsubFamilias, toast } from './utils.js';
 import { iniciarListenerFamilias } from './app.js';
-import { iniciarListenerSacolinhas } from './sacolinhas.js';
+import { iniciarListenerSacolinhas } from './sacolinhas.js?v=8';
 
 onAuthStateChanged(auth, user => {
   document.getElementById('loading-screen').style.display = 'none';
